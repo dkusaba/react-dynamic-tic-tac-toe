159c6b5a27b16105cbc5803fb2010880737d7201
Dice Kusaba <dicekusaba@gmail.com>
2023-10-30 04:57:33 -0700

    Finish left diagonal logic

--- src/utils/winArrayFinder.ts
+++ src/utils/winArrayFinder.ts
@@ -94,15 +94,15 @@ export const checkVertical = (gridArr, numWin: number) => {
 };
 
 export const checkLeftDiagonal = (gridArr, numWin) => {
-  // create arrays of diagonal lines
-  const linesArr = [];
+  // build arrays of diagonal lines
+  const diagArr = [];
   for (let i = 0; i < (gridArr.length - numWin) * 2 + 1; i++) {
     const diagonalCell: diagonalCell[] = [];
-    linesArr.push(diagonalCell);
+    diagArr.push(diagonalCell);
   }
-  for (let i = 0; i < Math.floor(linesArr.length / 2); i++) {
+  for (let i = 0; i < Math.floor(diagArr.length / 2); i++) {
     for (let x = 0; x < numWin + i; x++) {
-      linesArr[i].push({
+      diagArr[i].push({
         x: x,
         y: numWin - 1 + i - x,
         data: gridArr[numWin - 1 + i - x][x]
@@ -110,15 +110,15 @@ export const checkLeftDiagonal = (gridArr, numWin) => {
     }
   }
   for (let i = 0; i < gridArr.length; i++) {
-    linesArr[Math.floor(linesArr.length / 2)].push({
+    diagArr[Math.floor(diagArr.length / 2)].push({
       x: i,
       y: gridArr.length - 1 - i,
       data: gridArr[gridArr.length - 1 - i][i]
     });
   }
-  for (let i = 0; i < Math.floor(linesArr.length / 2); i++) {
+  for (let i = 0; i < Math.floor(diagArr.length / 2); i++) {
     for (let x = 0; x < gridArr.length - 1 - i; x++) {
-      linesArr[i + Math.ceil(linesArr.length / 2)].push({
+      diagArr[i + Math.ceil(diagArr.length / 2)].push({
         x: i + 1 + x,
         y: gridArr.length - 1 - x,
         data: gridArr[gridArr.length - 1 - x][i + 1 + x]
@@ -126,5 +126,48 @@ export const checkLeftDiagonal = (gridArr, numWin) => {
     }
   }
 
-  console.log(linesArr);
+  for (let i = 0; i < diagArr.length; i++) {
+    let count = 1;
+    for (let x = 0; x < diagArr[i].length - 1; x++) {
+      if (
+        typeof diagArr[i][x].data === 'string' &&
+        diagArr[i][x].data === diagArr[i][x + 1].data
+      ) {
+        count++;
+        if (count >= numWin) {
+          console.log('[count]:', count);
+          const data = [];
+          if (diagArr[i][x - 1]) {
+            for (let z = x; z >= 0; z--) {
+              if (diagArr[i][x - z].data === diagArr[i][x].data) {
+                data.push({
+                  x: diagArr[i][x - z].x,
+                  y: diagArr[i][x - z].y
+                });
+              }
+            }
+          }
+          if (diagArr[i][x + 2]) {
+            for (let z = x + 1; z < diagArr[i].length; z++) {
+              if (diagArr[i][x + 1].data === diagArr[i][z].data) {
+                data.push({
+                  x: diagArr[i][z].x,
+                  y: diagArr[i][z].y
+                });
+              }
+            }
+          }
+          console.log('[i]:', i);
+          console.log('[data]:', data);
+          console.log('A player won');
+          console.log(diagArr[i][x + 1].data === 'O' ? 'O' : 'X');
+          return data;
+        }
+      } else {
+        count = 1;
+      }
+    }
+  }
+
+  console.log(diagArr);
 };
