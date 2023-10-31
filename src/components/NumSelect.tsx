4b481fdf57fd5700d26327d7e21f6067d32d93bf
Dice Kusaba <dicekusaba@gmail.com>
2023-10-31 09:58:35 -0700

    center select

--- src/components/NumSelect.tsx
+++ src/components/NumSelect.tsx
@@ -17,7 +17,7 @@ const NumSelect: React.FC<NumSelectProps> = ({ num, grid, onChange }) => {
 
   return (
     <select
-      className='p-2 my-8 text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+      className='p-2 my-8 text-lg text-center bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
       onChange={onChange}
     >
       {buildOptions().map((option) => (
