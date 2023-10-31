7c3f2df20b62467896760651ab6206cf25ecb38d
Dice Kusaba <dicekusaba@gmail.com>
2023-10-30 21:43:00 -0700

    Sync router to github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-
 import './index.css';
+
+import Home from './pages/Home.tsx';
+import Game from './pages/Game.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/react-dynamic-tic-tac-toe/',
+    element: <App />,
+    children: [
+      {
+        path: '/react-dynamic-tic-tac-toe/',
+        element: <Home />
+      },
+      {
+        path: '/react-dynamic-tic-tac-toe/game',
+        element: <Game />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
